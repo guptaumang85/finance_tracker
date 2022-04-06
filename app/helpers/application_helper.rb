018d55bcfebc8243339416a2f0c5672f49029051
Umang Gupta <umanggupta.iet@gmail.com>
2022-04-07 01:41:48 +0530

    added flash and navigation bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def flash_class(level)
+    case level
+      when 'notice' then 'alert-primary'
+      when 'success' then 'alert-success'
+      when 'error' then 'alert-danger'
+      when 'alert' then 'alert-danger'
+    end
+  end
+
 end
